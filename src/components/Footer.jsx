58851ca1ab846e7a1b8f9c9635aefd487c47dad0
fcojr <fco-jr@live.com>
2018-01-09 21:34:45 -0200

    Using stateless Components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 const Footer = (props) =>{
     return(
         <div>
-            <footer><a href="https://github.com/fcojr/input-mirror"><strong>{props.name}</strong></a> hosted with ❤️ by <strong><a href="https://github.com">{this.props.host}</a></strong></footer>
+            <footer><a href="https://github.com/fcojr/input-mirror"><strong>{props.name}</strong></a> hosted with ❤️ by <strong><a href="https://github.com">{props.host}</a></strong></footer>
         </div>
     )
 }
